cb52025734b706a35a70af724c37bec118497b0e
Vepp1 <vinipontarolla@gmail.com>
2022-11-12 14:02:01 +0000

    Add login, logout and signup base template

--- static/script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  let datepicker = document.querySelectorAll('.datepicker');
-  M.Datepicker.init(datepicker, {
-    format: 'mmm dd, yyyy',
-    i18n: {done: "Select"}
-  });
-});
