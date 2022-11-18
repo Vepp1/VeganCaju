375aadc24209c6a2f4b9707cd58b0144b5005711
Vepp1 <vinipontarolla@gmail.com>
2022-11-18 14:56:51 +0000

    Add edit order functionality

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('make_order/', views.MakeOrder.as_view(), name='make_order'),
     path('my_orders/', views.ViewOrder.as_view(), name='my_orders'),
+    path('edit_order/<int:id>/', views.EditOrder.as_view(), name='edit_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -45,3 +45,40 @@ class ViewOrder(View):
         )
 
     
+class EditOrder(View):
+
+    def get(self, request, id, *args, **kwargs):
+        costumer = request.user.username
+        queryset = Order.objects.filter(costumer=costumer)
+        order = get_object_or_404(queryset, id=id)
+
+        return render(
+            request,
+            "edit_order.html", {"order_form": OrderForm(),
+            "order": order}
+        )
+
+    def post(self, request, id, *args, **kwargs):
+        order_form = OrderForm(data=request.POST)
+        costumer = request.user.username
+        queryset = Order.objects.filter(costumer=costumer)
+        order = get_object_or_404(queryset, id=id)
+        if order_form.is_valid():
+            order.size = order_form.cleaned_data['size']
+            order.flavor = order_form.cleaned_data['flavor']
+            order.pick_up = order_form.cleaned_data['pick_up']
+            order.save()
+            return redirect('my_orders')
+            
+            orderform = order_form.save(commit=False)
+            orderform.save()
+        else:
+            order_form = OrderForm()
+
+        return render(
+            request,
+            "index.html",
+            {
+                "order_form": OrderForm()
+            }
+        )
