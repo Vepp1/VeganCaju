977efe412afbfe41a85bb3f25dc3a173b9174513
Vepp1 <vinipontarolla@gmail.com>
2022-11-22 18:22:29 +0000

    Add modal to confirm that user wants to delete order

---
+++ static/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', function () {
+        myInput.focus()
+    })
+
+});
