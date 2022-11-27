092212373b14bc18d2b5eb1f9c3cc9a8746d6898
Vepp1 <vinipontarolla@gmail.com>
2022-11-27 17:59:41 +0000

    Fix bug in setting.py where , was inside ''

--- vegancaju/settings.py
+++ vegancaju/settings.py
@@ -29,9 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+DEBUG = True
 
 ALLOWED_HOSTS = ['vegancaju.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -47,7 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'cloudinary,'
+    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -156,13 +154,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
-STATIC_ROOT = ''
-
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = ( os.path.join('static'), )
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
